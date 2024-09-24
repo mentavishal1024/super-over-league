9178481ef91c8b1fa38666e29ce8692838ac3cb4
vishal <mentavishal1024@gmail.com>
2024-09-24 10:35:29 +0000

    super over league

---
+++ index.js
@@ -0,0 +1,16 @@
+const element = (
+  //   Write your code here.
+  <div class="background">
+    <h1 class="heading">Super Over League</h1>
+    <img
+      class="image"
+      src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+    />
+    <img
+      class="image"
+      src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+    />
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
